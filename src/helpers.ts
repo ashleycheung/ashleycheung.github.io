24da9c34b2db9457d16e22b64c9cd70fa8639b98
Ashley <ashleycheung2412@gmail.com>
2022-05-20 15:27:53 +1000

    Changed to react app

---
+++ src/helpers.ts
@@ -0,0 +1,16 @@
+
+export function getRootProperty(property: string): string {
+  return getComputedStyle(document.documentElement).getPropertyValue(property)
+}
+
+export function hexToRGB(hex: string, alpha: number | undefined) {
+  hex = hex.trim();
+  const r = parseInt(hex.slice(1, 3), 16),
+        g = parseInt(hex.slice(3, 5), 16),
+        b = parseInt(hex.slice(5, 7), 16);
+  if (alpha) {
+      return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+  } else {
+      return "rgb(" + r + ", " + g + ", " + b + ")";
+  }
+}
