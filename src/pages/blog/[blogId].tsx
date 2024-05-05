fa34ce3a87a806e7fc16bb651266036bac70b389
ashleycheung <ashleycheung2412@gmail.com>
2024-05-05 17:37:43 +1000

    seo tags

--- src/pages/blog/[blogId].tsx
+++ src/pages/blog/[blogId].tsx
@@ -26,6 +26,20 @@ export default function BlogPage({ blog }: { blog: Blog | undefined }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property="og:image" content={blog.thumbnailSrc} />
         <meta property="og:type" content="blog" />
+        <meta
+          property="og:url"
+          content={`https://www.ashleycheung.com/blog/${blog.id}`}
+        />
+        <meta property="og:title" content={blog.title} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="ashleycheung.com" />
+        <meta
+          property="twitter:url"
+          content={`https://www.ashleycheung.com/blog/${blog.id}`}
+        />
+        <meta name="twitter:title" content={blog.title} />
+        <meta name="twitter:description" content={blog.description} />
+        <meta name="twitter:image" content={blog.thumbnailSrc} />
         <link rel="icon" href="/favicon.svg" />
       </Head>
       <main>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,20 @@ export default function Home() {
         <title>Ashley Cheung</title>
         <meta name="description" content="Ashley Cheung's Personal Portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:image" content={"/assets/profile.png"} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={`https://www.ashleycheung.com`} />
+        <meta property="og:title" content={"Ashley Cheung"} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="ashleycheung.com" />
+        <meta property="twitter:url" content={`https://www.ashleycheung.com`} />
+        <meta name="twitter:title" content={"Ashley Cheung"} />
+        <meta
+          name="twitter:description"
+          content={"Ashley Cheung's Personal Portfolio"}
+        />
+        <meta name="twitter:image" content={"/assets/profile.png"} />
         <link rel="icon" href="/favicon.svg" />
       </Head>
       <main>
